b616569a8e21913435e72770787ffb2cb58d3cb1
chronowix <amelia.garcia.fern@gmail.com>
2025-10-16 15:57:45 +0200

    Modified app.js

--- app.js
+++ app.js
@@ -19,7 +19,19 @@ function renderBooks() {
     editBtn.textContent = "Modifier titre";
     editBtn.onclick = () => {
       // Insérer ici le code pour modifier le titre du livre
-        alert('Le bouton sert à rien :)');
+        let input = document.createElement('input');
+        input.type = 'text'; // typage de la variable input
+        input.value = book.title; // valeur de la variable (ici titre du livre)
+        let validateBtn = document.createElement('button'); //btn pour valider la modif
+        validateBtn.textContent = "Valider la modification";
+        validateBtn.onclick = () => { // ce qui se passe lorsqu'on clique sur Valider la modification
+            books[index].title = input.value; //change l'ancien titre avec le nouveau
+            renderBooks();
+        };
+        tr.innerHTML =''
+        tr.appendChild(input);
+        tr.appendChild(validateBtn);
+        //seule chose pas réussi: faire en sorte que le titre reste changé dans le localStorage après un refresh
     };
     actionsTd.appendChild(editBtn);
 
